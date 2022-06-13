b95bd4b68899b03f55c1ec49992e84ef9b91cb1a
Northernberg <Gustav.123@live.se>
2022-06-13 17:07:04 +0200

    Add new filter func for weather res

--- src/adapters/WeatherAdapter.ts
+++ src/adapters/WeatherAdapter.ts
@@ -1,9 +1,5 @@
 import axios, { AxiosResponse } from 'axios'
 
-const instance = axios.create({
-    baseURL: 'http://localhost:3001',
-})
-
 type Parameter = {
     name: string
     values: number[]
@@ -24,21 +20,12 @@ interface WeatherTimeseriesResponse {
     timeSeries: Timepoint[]
 }
 
-const filterParameters = (timepoint: Timepoint): ParsedTimepoint => {
-    return {
-        validTime: timepoint.validTime,
-        temp: timepoint.parameters.find((param) => param.name === 't')
-            ?.values[0],
-        icon: timepoint.parameters.find((param) => param.name === 'Wsymb2')
-            ?.values[0],
-    }
-}
-
+/** Function to reduce and filter out the results from the API */
 const reduceAndFilter = (
     total: Record<string, ParsedTimepoint[]>,
     current: Timepoint
 ): Record<string, ParsedTimepoint[]> => {
-    const date = new Date(current.validTime).getUTCDate()
+    const date = new Date(current.validTime).getDate()
     const parsedTimepoint = {
         validTime: current.validTime,
         temp: current.parameters.find((param) => param.name === 't')?.values[0],
@@ -48,17 +35,10 @@ const reduceAndFilter = (
     total = {
         ...total,
         [date]:
-            total[date] != undefined
+            total[date] !== undefined
                 ? [...total[date], parsedTimepoint]
                 : [parsedTimepoint],
     }
-    // let tempArray: ParsedTimepoint[] = []
-    // if (total[date] != undefined) {
-    //     tempArray = total[date]
-    // }
-    // tempArray.push(parsedTimepoint)
-
-    // total[date].push(tempArray)
 
     return total
 }
@@ -67,12 +47,9 @@ export const getWeatherTimeseries = async (
     lon: string,
     lat: string
 ): Promise<Record<string, ParsedTimepoint[]>> => {
-    const res: AxiosResponse<WeatherTimeseriesResponse> = await instance.get(
-        `/`,
-        { params: { lon, lat } }
+    const res: AxiosResponse<WeatherTimeseriesResponse> = await axios.get(
+        `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${lon}/lat/${lat}/data.json`
     )
-
-    //const filteredRes = res.data.timeSeries.map(filterParameters)
 
     const reducedRes = res.data.timeSeries.reduce(reduceAndFilter, {})
 
